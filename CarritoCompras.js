7175b2c50455b95683682e2e3f26816c64169e6c
GabrielaCedillo <gabychubis.cf@gmail.com>
2025-09-24 20:42:15 -0600

    Productos disponibles

---
+++ CarritoCompras.js
@@ -0,0 +1,7 @@
+// Lista de productos disponibles
+const productos = [
+    { nombre: 'Camisa', precio: 300 },
+    { nombre: 'Pantalón', precio: 500 },
+    { nombre: 'Zapatos', precio: 800 },
+    { nombre: 'Sombrero', precio: 200 }
+];
